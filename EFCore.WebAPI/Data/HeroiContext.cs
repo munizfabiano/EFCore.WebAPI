6e03cc5f9f2dc3a9cfd5ace3f8a2433d0fe72189
Domingos Fabiano de Souza Muniz <muniz.oliveira.rosangela@outlook.com.br>
2021-01-24 12:23:39 -0300

    Adicionar arquivos de projeto.

---
+++ EFCore.WebAPI/Data/HeroiContext.cs
@@ -0,0 +1,26 @@
+﻿using EFCore.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.WebAPI.Data
+{
+    public class HeroiContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+        public DbSet<HeroiBatalha> HeroiBatalhas { get; set; }
+        public DbSet<IdentidadeSecreta> IdentidadeSecretas { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-E999VKR\\SRVSQLSERVER2019;Initial Catalog=Curso_EntityFrameWorkCore;User ID=sa;Password=123456");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //indicação para o entity que é uma chave composta
+            modelBuilder.Entity<HeroiBatalha>(entity =>
+            {
+                entity.HasKey(e => new { e.BatalhaId, e.HeroiId });
+            });
+        }
+    }
+}
---
+++ EFCore.WebAPI/Models/IdentidadeSecreta.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCore.WebAPI.Models
+{
+    public class IdentidadeSecreta
+    {
+        public int Id { get; set; }
+        public string NomeReal { get; set; }
+        public Heroi Heroi { get; set; }
+        public int HeroiId { get; set; }
+    }
+}
